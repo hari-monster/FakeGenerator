4aa9286b0764489cd805f4adc9fb1beee07faf98
harinezumi <cc89mk@gmail.com>
2021-01-28 23:57:25 +0900

    first commit

---
+++ components/organisms/Footer/index.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+
+import { Wrapper } from './styles'
+
+const Footer: FC = () => {
+	return (
+		<Wrapper>
+			<p>sample footer</p>
+		</Wrapper>
+	)
+}
+
+export default Footer
---
+++ components/organisms/NavigationBar/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+import { Wrapper } from './styles'
+
+interface NavigationBarProps {}
+
+const NavigationBar: FC<NavigationBarProps> = () => {
+	return <Wrapper>Sample Navi</Wrapper>
+}
+
+export default NavigationBar
